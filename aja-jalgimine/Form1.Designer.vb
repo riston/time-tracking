c0078ccd24f2a89114303460f8489a9511d44f20
riston <est.risto@gmail.com>
2013-09-15 13:19:10 +0300

    Initial export

---
+++ aja-jalgimine/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.EventLabel = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(192, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Vali tuleviku sündmus"
+        '
+        'DateTimePicker
+        '
+        Me.DateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time
+        Me.DateTimePicker.Location = New System.Drawing.Point(16, 36)
+        Me.DateTimePicker.Name = "DateTimePicker"
+        Me.DateTimePicker.Size = New System.Drawing.Size(506, 20)
+        Me.DateTimePicker.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(122, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Sündmuseni:"
+        '
+        'EventLabel
+        '
+        Me.EventLabel.AutoSize = True
+        Me.EventLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EventLabel.Location = New System.Drawing.Point(140, 59)
+        Me.EventLabel.Name = "EventLabel"
+        Me.EventLabel.Size = New System.Drawing.Size(0, 24)
+        Me.EventLabel.TabIndex = 1
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 93)
+        Me.Controls.Add(Me.DateTimePicker)
+        Me.Controls.Add(Me.EventLabel)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents EventLabel As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ aja-jalgimine/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click, EventLabel.Click, Label2.Click
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker.ValueChanged
+        Dim dateTime As DateTime = DateTimePicker.Value
+
+        If DateDiff("s", dateTime, Now) < 0 Then
+            ' Picked is later than Now
+            Timer1.Enabled = True
+        Else
+            ' Now is later than picked
+            MessageBox.Show("Vali aeg tulevikust, mitte minevikust!")
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        DateTimePicker.Enabled = False
+
+        Dim currentDateTime As DateTime = TimeOfDay
+        Dim dateTime As DateTime = DateTimePicker.Value
+        Dim diff As TimeSpan = dateTime.Subtract(Now)
+
+        EventLabel.Text = diff.Days.ToString + " päeva " +
+            diff.Hours.ToString + " tundi " +
+            diff.Minutes.ToString + " minutit " +
+            diff.Seconds.ToString + " sekundit"
+
+
+    End Sub
+End Class
